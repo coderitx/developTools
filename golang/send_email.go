2303ad26915b7557de5a6d73bcbb9a4475aff00e
scliang-strive <54300717+scliang-strive@users.noreply.github.com>
2023-04-23 22:53:49 +0800

    add send email tools

---
+++ golang/send_email.go
@@ -0,0 +1,33 @@
+package tools
+
+var (
+	CodeSize = 6
+	EmailPassword="xxxxx"
+)
+
+// SendCodeToEmail
+// 发送验证码到邮箱
+func SendCodeToEmail(mail, code string) error {
+	e := email.NewEmail()
+	e.From = "golang-developer <$email>"
+	e.To = []string{mail}
+	e.Subject = "验证码请求"
+	e.HTML = []byte("<h3>你的验证码为: " + code + "</h3>")
+	err := e.SendWithTLS("smtp.163.com:465", smtp.PlainAuth("", "$email", EmailPassword, "smtp.163.com"),
+		&tls.Config{InsecureSkipVerify: true, ServerName: "smtp.163.com"})
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+// RandCode
+// 生成随机验证码
+func RandCode() string {
+	code := ""
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < CodeSize; i++ {
+		code += fmt.Sprintf("%d", rand.Intn(10))
+	}
+	return code
+}
