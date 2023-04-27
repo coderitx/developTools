74bbdb9b3c34084ed8c68ef3c8a6bfaabf1eb4c6
scliang-strive <54300717+scliang-strive@users.noreply.github.com>
2023-04-27 10:18:34 +0800

    Create get_local_ip.go
    获取本机ip地址

---
+++ golang/get_local_ip.go
@@ -0,0 +1,20 @@
+package tools
+
+
+import (
+    "fmt"
+    "net"
+)
+
+// GetIp 获取ip
+func GetIp() (string,error) {
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+        return "",err
+    }
+    for _, addr := range addrs {
+        if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+          return ipnet.IP.String(),nil 
+        }
+    }
+}
