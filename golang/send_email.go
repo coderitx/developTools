ce9170b568ccb4a55e8bc26eb952f94d9828d0b2
coderitx <54300717+coderitx@users.noreply.github.com>
2023-05-24 08:51:06 +0800

    update send email
    更新发送邮件

--- golang/send_email.go
+++ golang/send_email.go
@@ -1,35 +1,79 @@
 package tools
 
-var (
-	// 验证码长度
-	CodeSize = 6
-	// 邮箱密钥
-	EmailPassword="xxxxx"
+import (
+	"gopkg.in/gomail.v2"
+	"os"
 )
 
-// SendCodeToEmail
-// 发送验证码到邮箱
-func SendCodeToEmail(mail, code string) error {
-	e := email.NewEmail()
-	e.From = "golang-developer <$email>"
-	e.To = []string{mail}
-	e.Subject = "验证码请求"
-	e.HTML = []byte("<h3>你的验证码为: " + code + "</h3>")
-	err := e.SendWithTLS("smtp.163.com:465", smtp.PlainAuth("", "$email", EmailPassword, "smtp.163.com"),
-		&tls.Config{InsecureSkipVerify: true, ServerName: "smtp.163.com"})
-	if err != nil {
-		return err
-	}
-	return err
+type Email struct {
+	Host             string `json:"host" yaml:"host"`
+	Port             int    `json:"port" yaml:"port"`
+	User             string `json:"user" yaml:"user"` // 发件人邮箱
+	Password         string `json:"password" yaml:"password"`
+	DefaultFromEmail string `json:"default_from_email" yaml:"default-from-email"` // 默认的发件人名字
+	UseSSL           bool   `json:"use_ssl" yaml:"use_ssl"`                       // 是否使用ssl
+	UserTls          bool   `json:"user_tls" yaml:"user_tls"`
 }
 
-// RandCode
-// 生成随机验证码
-func RandCode() string {
-	code := ""
-	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < CodeSize; i++ {
-		code += fmt.Sprintf("%d", rand.Intn(10))
+type Subject string
+
+const (
+	Code  Subject = "平台验证码"
+	Note  Subject = "操作通知"
+	Alarm Subject = "告警通知"
+)
+
+type Api struct {
+	Subject Subject
+}
+
+func NewEmail() Email {
+	return Email{}
+}
+
+func (a Api) Send(name, body string) error {
+	return send(name, string(a.Subject), body)
+}
+
+func NewCode() Api {
+	return Api{
+		Subject: Code,
 	}
-	return code
+}
+func NewNote() Api {
+	return Api{
+		Subject: Note,
+	}
+}
+func NewAlarm() Api {
+	return Api{
+		Subject: Alarm,
+	}
+}
+
+// send 邮件发送  发给谁，主题，正文
+func send(name, subject, body string) error {
+	e := NewEmail()
+	password := os.Getenv("EmailPwd")
+	return sendMail(
+		e.User,
+		e.password,
+		e.Host,
+		e.Port,
+		name,
+		e.DefaultFromEmail,
+		subject,
+		body,
+	)
+}
+
+func sendMail(userName, authCode, host string, port int, mailTo, sendName string, subject, body string) error {
+	m := gomail.NewMessage()
+	m.SetHeader("From", m.FormatAddress(userName, sendName)) // 谁发的,发送标题
+	m.SetHeader("To", mailTo)                                // 发送给谁
+	m.SetHeader("Subject", subject)                          // 主题
+	m.SetBody("text/html", body)
+	d := gomail.NewDialer(host, port, userName, authCode)
+	err := d.DialAndSend(m)
+	return err
 }
